67a1e31e8d2bd4cb8cd7c1e66c7bbcd95289ff26
thor56 <38258444+thor56@users.noreply.github.com>
2020-04-22 16:56:43 +0530

    basic

--- flaskblog.py
+++ flaskblog.py
@@ -20,43 +20,32 @@ posts = [
         'date_posted': 'April 21, 2018'
     }
 ]
-tweets_lis = []
-tweet_dict = {}
-consumer_key = 'SgJJpWvvQMB0QU2nrRM6by2DM'
-consumer_key_secret = '8aRUfDlqzuoOnY5yaGrcwkStVqoDvxo6aTutXM8LlNfkG0BUZz'
-access_token = '784635562946887681-Gc2iiIEa8QCMxSX9fIUClBARsTBGXdo'
-access_token_secret = 'zbBip5pTugOsMtXlZtGVxlnf8mgEOFlOoJFhLqk2HzlAX'
-auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-# search_term = input("Enter a search keyword: ")
-search_term = "donald"
-public_tweets = api.search(search_term)
-res = public_tweets
-userdata = { "data":[]}
+twdata = []
+
+def TweetBlast(searchterm):
+    twdata.clear()
+    consumer_key = 'SgJJpWvvQMB0QU2nrRM6by2DM'
+    consumer_key_secret = '8aRUfDlqzuoOnY5yaGrcwkStVqoDvxo6aTutXM8LlNfkG0BUZz'
+    access_token = '784635562946887681-Gc2iiIEa8QCMxSX9fIUClBARsTBGXdo'
+    access_token_secret = 'zbBip5pTugOsMtXlZtGVxlnf8mgEOFlOoJFhLqk2HzlAX'
+    auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    # search_term = input("Enter a search keyword: ")
+    search_term = searchterm
+    public_tweets = api.search(search_term)
+
+    for tweet in public_tweets:
+        analysis = TextBlob(tweet.text)
+        user = {}
+        user["text"]=tweet.text
+        user["analysis"]=analysis.sentiment
+        add_user(user)
+    return twdata
 
 def add_user(user):
-  userdata["data"].append(user)
+  twdata.append(user)
 
-for tweet in public_tweets:
-    tweet_dict['text'] = tweet.text
-    analysis = TextBlob(tweet.text)
-    tweet_dict['ana'] = analysis.sentiment
-    tweets_lis.append(tweet_dict)
-    user = {}
-    user["name"]=tweet.text
-    user["age"]=analysis.sentiment
-    add_user(user)
-    # print(tweets_lis)
-    if analysis.sentiment[0]>0:
-            print ('Positive')
-    else:
-            print ('Negative')
-    print("")
-
-
-
-#print(userdata)
 
 @app.route("/")
 @app.route("/home")
@@ -84,9 +73,11 @@ def register():
 def search():
     form = SearchForm()
     if form.validate_on_submit():
-        flash(f'Sentiment analysis for phrase {form.searchphrase.data}!', 'success')
-        session['my_list'] = tweets_lis
-        print(tweets_lis)
+        
+        tweet_lis = TweetBlast(form.searchphrase.data)
+        flash(f'Sentiment analysis for phrase {form.searchphrase.data}', 'success')
+        session['my_list'] = tweet_lis
+        session.modified = True
         return redirect(url_for('result'))
     return render_template('search.html', title='Search', form=form)
 
